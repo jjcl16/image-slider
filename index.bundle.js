a03a1dbc6564e236cc1126c0138e15daaf28ea6a
jjcl16 <caraballo2j@gmail.com>
2023-07-09 01:18:26 -0300

    showing images and working the buttons

---
+++ index.bundle.js
@@ -0,0 +1,20 @@
+(self["webpackChunkimage_slider"] = self["webpackChunkimage_slider"] || []).push([["index"],{
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ (() => {
+
+console.log("hola");
+console.log("2");
+
+/***/ })
+
+},
+/******/ __webpack_require__ => { // webpackRuntimeModules
+/******/ var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+/******/ var __webpack_exports__ = (__webpack_exec__("./src/index.js"));
+/******/ }
+]);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguYnVuZGxlLmpzIiwibWFwcGluZ3MiOiI7Ozs7Ozs7O0FBQUE7QUFDQSIsInNvdXJjZXMiOlsid2VicGFjazovL2ltYWdlLXNsaWRlci8uL3NyYy9pbmRleC5qcyJdLCJzb3VyY2VzQ29udGVudCI6WyJjb25zb2xlLmxvZyhcImhvbGFcIik7XHJcbmNvbnNvbGUubG9nKFwiMlwiKTsiXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=
