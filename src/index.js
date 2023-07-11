677b5dbfa77ddc58836e456f0bf5170ea16c82c2
jjcl16 <caraballo2j@gmail.com>
2023-07-10 21:42:16 -0300

    only rest function to autochange image

--- src/index.js
+++ src/index.js
@@ -6,6 +6,11 @@ function importAll(r) {
   return images;
 }
 
+// Imagen loader
+//import images from "../images/pruebaa.jpeg";
+const images = importAll(require.context('../images', false, /\.(png|jpe?g|svg)$/));
+const imagesNames = Object.keys(images);
+//
 
 // Creating DOM
 /*
@@ -28,16 +33,20 @@ const imageContainer = document.createElement("div");
 imageContainer.setAttribute("id","imageContainer");
 const leftButton = document.createElement("button");
 leftButton.setAttribute("id", "leftButton");
-leftButton.textContent = "<";
+leftButton.textContent = "↤";
 leftButton.addEventListener("click", goLeft);
 const rightButton = document.createElement("button");
 rightButton.setAttribute("id", "rightButton");
-rightButton.textContent = ">";
+rightButton.textContent = "↦";
 rightButton.addEventListener("click", goRight);
+const dotsContainer = document.createElement("div");
+dotsContainer.setAttribute("id", "dotsContainer");
+
 
 generalContainer.appendChild(imageContainer);
 generalContainer.appendChild(leftButton);
 generalContainer.appendChild(rightButton);
+generalContainer.appendChild(dotsContainer);
 
 const slidebar = document.createElement("div");
 slidebar.setAttribute("id", "slidebar");
@@ -50,50 +59,140 @@ body.appendChild(slidebar);
 const imagen = document.createElement("img");
 imagen.setAttribute("id", "imageDisplayed");
 
-// Imagen loader
-//import images from "../images/pruebaa.jpeg";
-const images = importAll(require.context('../images', false, /\.(png|jpe?g|svg)$/));
-const imagesNames = Object.keys(images);
-//
+//load image
 
 imagen.setAttribute("src", images[imagesNames[0]].default);
+imagen.classList.add("fade");
 imageContainer.appendChild(imagen);
 
 
 const footer = document.createElement("div");
 footer.setAttribute("id", "footer");
 body.appendChild(footer);
+//console.log(imagesNames[0]);
+removeFade();
+/*  Creating slidebar                                                                                     */
 
+imagesNames.forEach(element => {
+  loadSliderImages(element);
+  loadDots();    
+});
 
+function loadSliderImages(element){
+  const slidebarImgContainer = document.createElement("div");
+  slidebarImgContainer.classList.add("slidebarImgContainer");
+  slidebarImgContainer.addEventListener("click", selectPicture);
+  const newImagetoAdd = document.createElement("img");
+  newImagetoAdd.classList.add("slidebarImage");
+  newImagetoAdd.setAttribute("src", images[element].default);
+  slidebarImgContainer.appendChild(newImagetoAdd)
+  slidebar.appendChild(slidebarImgContainer);
+}
+
+function loadDots(){
+  const oneDotContainer = document.createElement("div");
+  oneDotContainer.classList.add("dot");
+  oneDotContainer.textContent = "·";
+  dotsContainer.appendChild(oneDotContainer);
+}
+
+//Refresh dots
+dotPicture(imagesNames[0]);
+slidebarImageShowing(imagesNames[0]);
+
+function dotPicture(pictureName){
+ const index = imagesNames.indexOf(pictureName);
+ const dots = dotsContainer.childNodes;
+ dots[index].classList.add("dotSelected");
+}
+
+function removeSelectedDot(){
+  const dots = dotsContainer.childNodes;
+  dots.forEach(element => {
+    element.classList.remove("dotSelected");
+  })
+}
+
+function slidebarImageShowing(pictureName){
+  const index = imagesNames.indexOf(pictureName);
+  const slidebarImgContainers = slidebar.childNodes;
+  slidebarImgContainers[index].classList.add("slidebarImageShowing");
+}
+
+function removeSlidebarImageShowing(){
+  const slidebarImgContainers = slidebar.childNodes;
+  slidebarImgContainers.forEach(element => {
+    element.classList.remove("slidebarImageShowing");
+  })
+}
+
+/** event functions  */
 function goLeft(){
-  const imageName = checkImageShowing();
+  removeSelectedDot();
+  removeSlidebarImageShowing();
+  const imageName = checkImageName(imagen.src);//checkImageShowing();
   const indexOfImage = imagesNames.indexOf(imageName);
+  let newImage = "";
   if (indexOfImage == 0) {
     const lastImages = imagesNames.length - 1;
-    const newImage = imagesNames[lastImages];
+    newImage = imagesNames[lastImages];
     imagen.setAttribute("src", images[newImage].default);
   }else{
-    const newImage = imagesNames[indexOfImage - 1]
+    newImage = imagesNames[indexOfImage - 1]
     imagen.setAttribute("src", images[newImage].default);
   }
+  imagen.classList.add("fade");
+  removeFade();
+  dotPicture(newImage);
+  slidebarImageShowing(newImage);
 }
 
 function goRight(){
-  const imageName = checkImageShowing();
+  removeSelectedDot();
+  removeSlidebarImageShowing();
+  const imageName = checkImageName(imagen.src);//checkImageShowing();
   const indexOfImage = imagesNames.indexOf(imageName);
   const lastImage = imagesNames.length - 1;
+  let newImage = "";
   if (indexOfImage == lastImage) {
-    const newImage = imagesNames[0];
+    newImage = imagesNames[0];
     imagen.setAttribute("src", images[newImage].default);
   }else{
-    const newImage = imagesNames[indexOfImage + 1]
+    newImage = imagesNames[indexOfImage + 1]
     imagen.setAttribute("src", images[newImage].default);
   }
+  imagen.classList.add("fade");
+  removeFade();
+  dotPicture(newImage);
+  slidebarImageShowing(newImage);
 }
 
-function checkImageShowing(){
-  const splittedImageSource = imagen.src.split("/");
+function checkImageName(source){
+
+  const splittedImageSource = source.split("/");//imagen.src.split("/");
   const Long = splittedImageSource.length;
   const imageName = splittedImageSource[Long - 1];
   return imageName;
+}
+
+function removeFade(){
+  setTimeout(function() {
+    imagen.classList.remove("fade");
+  }, 1000);
+}
+
+function selectPicture(e){
+  removeSelectedDot();
+  removeSlidebarImageShowing();
+  let imageName;
+  if(e.target.childNodes.length){
+    imageName = checkImageName(e.target.childNodes[0].src);
+  }else{
+    imageName = checkImageName(e.target.src);
+  }
+  imagen.setAttribute("src", images[imageName].default);
+  imagen.classList.add("fade");  
+  dotPicture(imageName);
+  slidebarImageShowing(imageName);
+  removeFade();
 }
